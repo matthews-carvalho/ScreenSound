fe2313c19c92b7d069b45405f7a4449d22166892
matthews-carvalho <matthews.carvalho@hotmail.com>
2025-02-28 10:46:24 -0300

    Implement the main menu
    The main functionalities of the main menu include:
    - Register a band
    - Display registered bands
    - Rate a band
    - Display a band's details
    
    These changes enable users to interact with all the functionalities of the application.

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, World");
+﻿void Menu()
+{
+    Console.WriteLine("1. Register Band");
+    Console.WriteLine("2. Display Bands");
+    Console.WriteLine("3. Rate Band");
+    Console.WriteLine("4. Display Band Details");
+    Console.WriteLine("0. Exit");
+
+    Console.Write("Choose an option: ");
+    string option = Console.ReadLine()!;
+    int intOption = int.Parse(option);
+
+    switch (intOption)
+    {
+        case 1:
+            Console.WriteLine("Register Band");
+            break;
+        case 2:
+            Console.WriteLine("Display Bands");
+            break;
+        case 3:
+            Console.WriteLine("Rate Band");
+            break;
+        case 4:
+            Console.WriteLine("Display Band Details");
+            break;
+        case 0:
+            return;
+        default:
+            Console.WriteLine("Invalid option");
+            break;
+    }
+}
+
+Menu();
